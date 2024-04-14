98f995a3e3830e6044798115e6398c886d7dbcbc
Yudai Yaguchi <uwyudai@gmail.com>
2024-04-13 18:52:57 -0700

    connect chatend point to server

--- src/api/backend/voiceChat.js
+++ src/api/backend/voiceChat.js
@@ -1,7 +1,6 @@
 export const getResponse = async (body) => {
-  const url = 'http://127.0.0.1:5001/yudai-portfolio/us-central1/voice_chat';
   try {
-    const response = await fetch(url, {
+    const response = await fetch(process.env.REACT_APP_CHAT_URL, {
       method: 'POST',
       body: body,
       headers: {
